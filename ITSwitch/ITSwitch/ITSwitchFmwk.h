d4837182e4ef3ca919c821578624c0c0421c3554
Florian Friedrich <ffried@me.com>
2018-08-02 13:51:35 +0200

    Add framework project

---
+++ ITSwitch/ITSwitch/ITSwitchFmwk.h
@@ -0,0 +1,19 @@
+//
+//  ITSwitchFmwk.h
+//  ITSwitch
+//
+//  Created by Florian Friedrich on 02.08.18.
+//  Copyright © 2018 Ilija Tovilo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ITSwitch.
+FOUNDATION_EXPORT double ITSwitchVersionNumber;
+
+//! Project version string for ITSwitch.
+FOUNDATION_EXPORT const unsigned char ITSwitchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ITSwitch/PublicHeader.h>
+#import <ITSwitch/ITSwitch.h>
+
